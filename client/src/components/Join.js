080f33e754369c70b01550ad7bb6dfd312ad8356
Ananya09022002 <ananyanigam09022002@gmail.com>
2024-01-15 22:52:59 +0530

    socket.io connection

--- client/src/components/Join.js
+++ client/src/components/Join.js
@@ -1,8 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
+import { Link } from 'react-router-dom';
 
 const Join = () => {
+    const [name, setName] = useState('');
+    const [Room, setRoom] = useState('');
+
     return (
-        <h1>JOIN</h1>
+        <div>
+            <div>
+                <h1>Join</h1>
+                <div>
+                    <div><input placeholder="Name" className="joinInput" type="text" onChange={(event) => setName(event.target.value)} /></div>
+                    <div><input placeholder="Room" className="joinInput" type="text" onChange={(event) => setRoom(event.target.value)} /></div>
+                    <Link onClick={(event) => (!name | !room) ? event.preventDefault() : null} to={`/chat?name=${name}&room=${room}`}>
+                    <button className="button" type="submit">Sign In</button>
+                    </Link>
+                </div>
+            </div>
+        </div>
     )
 }
 
